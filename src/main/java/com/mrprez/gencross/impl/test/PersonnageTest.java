4c7d49c58f063122214d18e3861471d03fdf731a
mrprez <mrprez@mrprez-PC>
2021-04-25 18:08:13 +0200

    Ajout du code

---
+++ src/main/java/com/mrprez/gencross/impl/test/PersonnageTest.java
@@ -0,0 +1,14 @@
+package com.mrprez.gencross.impl.test;
+
+import com.mrprez.gencross.Personnage;
+
+public class PersonnageTest extends Personnage {
+	
+	@Override
+	public void calculate() {
+		super.calculate();
+	}
+	
+	
+
+}
